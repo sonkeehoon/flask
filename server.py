6c6d2567f9e961ce240194b73b57a3726c2bf954
kee_hoon <djfkfk12345@naver.com>
2022-10-16 00:28:46 +0900

    221016

--- server.py
+++ server.py
@@ -1,8 +1,41 @@
 from flask import Flask
+import random
 
 app=Flask(__name__)
 
+
+topics=[
+    {'id':1, 'title':'html', 'body':'html is ...'},
+    {'id':2, 'title':'css', 'body':'css is ...'},
+    {'id':3, 'title':'javascript', 'body':'javascript is ...'}
+]
+# topics 리스트대신 어떤 db에서 데이터를 불러오는것이 목표 (나중에)
+
 @app.route('/')
 def index():
-    return 'hi'
+    liTags=''
+    for topic in topics:
+        liTags=liTags+f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+        
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a><h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>
+    '''
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+@app.route('/read/<id>/')
+def read(id):
+    print(id)
+    return 'Read '+id
 app.run(port=5001, debug=True)
