6e45204a08d82d8a98d4cbea483c689e9e354e6a
kee_hoon <djfkfk12345@naver.com>
2022-10-16 23:14:08 +0900

    221016

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,request,redirect
 import random
 
 app=Flask(__name__)
 
 
+nextId=4
 topics=[
     {'id':1, 'title':'html', 'body':'html is ...'},
     {'id':2, 'title':'css', 'body':'css is ...'},
@@ -11,31 +12,66 @@ topics=[
 ]
 # topics 리스트대신 어떤 db에서 데이터를 불러오는것이 목표 (나중에)
 
-@app.route('/')
-def index():
-    liTags=''
-    for topic in topics:
-        liTags=liTags+f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
-        
+def template(contents, content):
     return f'''<!doctype html>
     <html>
         <body>
             <h1><a href="/">WEB</a><h1>
             <ol>
-                {liTags}
+                {contents}
             </ol>
-            <h2>Welcome</h2>
-            Hello, Web
+            {content}
+            <ul>
+                <li><a href="/create/">create</a></li>
+            </ul>
         </body>
     </html>
     '''
+def getContents():
+    liTags=''
+    for topic in topics:
+        liTags=liTags+f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
 
-@app.route('/create/')
-def create():
-    return 'Create'
+@app.route('/')
+def index():
+        
+    return template(getContents(),'<h2>Welcome</h2>Hello, WEB')
 
-@app.route('/read/<id>/')
+@app.route('/read/<int:id>/')
 def read(id):
-    print(id)
-    return 'Read '+id
+    
+    title=''
+    body=''
+    for topic in topics:
+        if id==topic['id']:
+            title=topic['title']
+            body=topic['body']
+            break
+
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+
+@app.route('/create/',methods=['GET','POST'])
+def create():
+    print('request.method',request.method)
+    if request.method=="GET": 
+        content='''
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+        return template(getContents(),content)
+    elif request.method=="POST":
+        global nextId
+        title=request.form['title'] 
+        body=request.form['body']
+        newTopic={"id":nextId,"title":title,"body":body}
+        topics.append(newTopic)
+        url='/read/'+str(nextId)+'/'
+        nextId=nextId+1
+        return redirect(url)
+
+
 app.run(port=5001, debug=True)
